3118ef60d06e0eb0ad686c85b7cb15cf53b0c094
Duncan Temple Lang <duncan@r-project.org>
2021-01-17 16:38:28 -0800

    defined the sig

--- tests/pointer.R
+++ tests/pointer.R
@@ -5,9 +5,11 @@ library(Rffi)
 # So we can compare the input and the result and they should have the same value.
 
 fun = getNativeSymbolInfo("R_ptr")$address
+sig = CIF(pointerType, list(pointerType))
 ans = callCIF(sig, fun, fun, returnInputs = FALSE)
 
 # Are the values of fun and ans the same, up to the attributes.
+# Can't use unclass() on an externalptr
 attr(fun, "class") = NULL
 
 stopifnot(identical(fun, ans))
